d982a0f1b9f6936545571695888d03686aa079e7
timothydhoe <timothy.dhoe@gmail.com>
2025-10-10 13:19:54 +0200

    setup structure of the programme

---
+++ src/convert_temp.py
@@ -0,0 +1,27 @@
+def main():
+    """
+    Programme that converts Celsius to Fahrenheit and the otherway around.
+    Formula: C = (5/9) * (F - 32)
+
+    User input is an integer (temp) and a string(C or F).
+    Programme prints out the converted temperature.
+    """
+    temp = get_temp()
+
+    # if temp is in Celcius:
+        # converted_temp = c_to_f(temp)
+    # else:
+        # converted_temp f_to_c(temp)
+
+    print(converted_temp)
+
+def get_temp():
+    pass
+
+def c_to_f(temp):
+    pass
+
+def f_to_c(temp):
+    pass
+
+main()
